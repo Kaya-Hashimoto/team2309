c94db655d0cbc7213f3c049795548501f89b3398
kaya <kaaya.xx.x@gmail.com>
2023-11-14 16:18:26 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
+name = Faker::Artist.name,
+email = Faker::Internet.email,
+password = Faker::Alphanumeric.alpha(number: 8),
+title = Faker::ProgrammingLanguage.name
+contents = Faker::Quotes::Shakespeare.romeo_and_juliet_quote
+
 5.time do
-  name = Faker::Artist.name,
-  email = Faker::Internet.email,
-  password = Faker::Alphanumeric.alpha(number: 8),
-
   user = User.create!(
-
-  )
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password)
+  Task.create!(title: title, contents: contents)
 end
+
+User.create!(name: 'Dpro2309', email: 'dpro@xx.mail',
+            password: 'Dpro2309', password_confirmation: 'Dpro2309',
+            title: title, contents: contents)
